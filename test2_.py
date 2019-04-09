182b64f8ccf5c5ba3ba49dc4c568cc3ae59a15d2
jherna66 <jherna66@ucsc.edu>
2019-04-09 14:01:14 -0700

    Delete test2_.py

--- test2_.py
+++
@@ -1,8 +0,0 @@
-import inspect, os
-print inspect.getfile(inspect.currentframe()) # script filename (usually with path)
-thisFile = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) # script directory
-print thisFile
-
-import sys
-sys.path.append(thisFile)
-from Analyze_V1 import learn
